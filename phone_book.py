617810e34d109b9e7c7aa6ab1073e4643a2e523c
anekha sokhal <anekha17@yahoo.com>
2023-01-28 12:47:17 +0400

    CSV Files

--- phone_book.py
+++ phone_book.py
@@ -1,1 +1,14 @@
+import csv
 
+#with open('data/phone_book.csv', mode='r') as csv_file:
+    #csv_reader = csv.reader(csv_file)
+    #line_count = 0
+    #for row in csv_reader:
+        #if line_count !=0:
+            #print(row[1] + ' : ' + row[2])
+        #line_count += 1
+
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        print(row['first_name'] + ': ' + row['phone_number'])
